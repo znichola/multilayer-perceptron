44419fddc12201231c2193ae8701fb4561ecf125
Nicholas Zivkovic <znichola@student.42nice.fr>
2025-09-16 17:22:02 +0200

    remove id colum

--- common.py
+++ common.py
@@ -6,8 +6,8 @@ import pandas as pd
 def load_data(file_path: str = "data.csv") -> Optional[pd.DataFrame]:
     try:
         data = pd.read_csv(file_path, header=None)
-        col_names = ["diagnosis" if i == 1 else f"f{i + (0 if i >= 1 else 1)}"
-            for i in range(data.shape[1])]
+        data = data.drop(data.columns[0], axis=1) # Drop the first column (ID)
+        col_names = ["diagnosis"] + [f"f{i}" for i in range(1, data.shape[1])]
         return data.set_axis(col_names, axis=1)
     except OSError:
         print(f"Error: Could not open file '{file_path}'")
--- main.py
+++ main.py
@@ -5,32 +5,14 @@ import pandas as pd
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 
-
-def loadData(file_path: str = "data.csv") -> Optional[pd.DataFrame]:
-    try:
-        data = pd.read_csv(file_path, header=None)
-        return data
-    except OSError:
-        print(f"Error: Could not open file '{file_path}'")
-        return None
-    except pd.errors.ParserError:
-        print("Error: File contents are not valid or improperly formatted")
-        return None
-
-
-def appendHeader(df: DataFrame) -> DataFrame:
-    col_names = ["diagnosis" if i == 1 else f"f{i + (1 if i >= 1 else 0)}"
-                 for i in range(df.shape[1])]
-    return df.set_axis(col_names, axis=1)
-
+import common as cm
 
 def main():
     args = parser.parse_args()
 
-    df = loadData(args.file_path)
+    df = cm.load_data(args.file_path)
     if df is None:
         return
-    df = appendHeader(df)
 
     print("\nData overview")
     print(df)
